b04f4ad15c29ac79ea3ca8ec07afff006db6aa83
Rolando Parada <rolandoparadapuig@gmail.com>
2022-07-26 16:09:45 -0400

    project Created

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "../src/css/app.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <div>hello</div>
+    </div>
+  );
+}
+
+export default App;
